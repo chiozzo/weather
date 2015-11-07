12920f1c99492d3f0a5720f2a96cc57804691350
Kirk Graves <graves.kirk@gmail.com>
2015-11-07 10:54:16 -0600

    created authentication module

---
+++ javascripts/authenticate.js
@@ -0,0 +1,41 @@
+define(function(require) {
+	var $ = require('jquery');
+	var loginRef = new Firebase('https://local-weather-mk.firebaseIO.com');
+	
+
+	return {
+
+		loginUser: function(email, password) {
+
+			var loginForm = {
+				email: email,
+				password: password
+			};
+
+			loginRef.authWithPassword(loginForm, function(error, authData) {
+				if (error) {
+					console.log('Login failed!', error);
+				}
+				else {
+					console.log('Authenticated succesfully with payload!', authData);				
+				}
+			});
+
+		},
+
+		registerUser: function() {
+			ref.createUser({
+  				email    : $('#email').val(),
+  				password : $('#password').val()
+				}, function(error, userData) {
+  				if (error) {
+    				console.log("Error creating user:", error);
+  			} else {
+    			console.log("Successfully created user account with uid:", userData.uid);
+  			}
+			});		
+		});
+	
+	};
+
+});
