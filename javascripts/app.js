ed37cba5b99729bedc882927bb53da40a86a3702
Kirk Graves <graves.kirk@gmail.com>
2015-11-09 14:14:32 -0600

    fixed gutters between forecast and saved weather divs and added Scotch Panels to project

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,22 +7,40 @@ requirejs.config({
     "firebase": "../lib/bower_components/firebase/firebase",
     "material": "../lib/bower_components/bootstrap-material-design/dist/js/material.min",
     "q": "../lib/bower_components/q/q",
-    "bootstrap-material-design": "../lib/bower_components/bootstrap-material-design/dist/js/material.min"
+    "bootstrap-material-design": "../lib/bower_components/bootstrap-material-design/dist/js/material.min",
+    "scotch-panels": "../lib/bower_components/scotch-panels/dist/scotchPanels.min"
   },
   shim: {
     "bootstrap": ["jquery"],
     "material": ["bootstrap"],
+    "scotch-panels": ["jquery"],
     "firebase": {exports: "Firebase"}
   }
 });
 
-require(["jquery", "firebase", "hbs", "authenticate", "bootstrap", "material", "validate", "weatherView", "getWeather", "convertDT"],
-  function($, firebase, handlebars, authenticate, bootstrap, material, validate, weatherView, getWeather, convertDT) {
+require(["jquery", "firebase", "hbs", "authenticate", "bootstrap", "material", "scotch-panels", "validate", "weatherView", "getWeather", "convertDT"],
+  function($, firebase, handlebars, authenticate, bootstrap, material, scotchPanels, validate, weatherView, getWeather, convertDT) {
 
 	$.material.init();
+
+	var panelContainer = $('#panelContainer').scotchPanel({
+    containerSelector: 'body', // Make this appear on the entire screen
+    direction: 'left', // Make it toggle in from the left
+    duration: 900, // Speed in ms how fast you want it to be
+    transition: 'ease', // CSS3 transition type: linear, ease, ease-in, ease-out, ease-in-out, cubic-bezier(P1x,P1y,P2x,P2y)
+    clickSelector: '.toggle-panel', // Enables toggling when clicking elements of this class
+    distanceX: '100%', // Size for the toggle
+    enableEscapeKey: true // Clicking Esc will close the panel
+	});
+
 	
+
 	$('#currentWeatherView').hide();
+
 	$(document).ready(function(){
+		
+		panelContainer.open();
+		panelContainer.close();
 		
 		authenticate.loginUser('mncross@gmail.com', 'abc');
 
